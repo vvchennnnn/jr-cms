e7328d58ed32701d531d1156683008685d0386d2
chen hong <vvchennnnn@gmail.com>
2021-07-02 22:59:25 +1000

    route index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const studentRoute = require('./students');
-// const teacherRoute = require('./teachers');
+const teacherRoute = require('./teachers');
 const courseRoute = require('./courses');
 
 const router = express.Router();
 
 router.use('/students', studentRoute);
-// router.use('/teachers', teacherRoute);
+router.use('/teachers', teacherRoute);
 router.use('/courses', courseRoute);
 
 module.exports = router;
