506debdbfa264fa5a88d87fe2f779506b5d75ef5
chen hong <vvchennnnn@gmail.com>
2021-07-03 12:17:20 +1000

    teacher controller framework

---
+++ src/controllers/teachers.js
@@ -0,0 +1,16 @@
+function getAllTeachers(req, res) {
+  res.json([]);
+}
+
+function getTeacherById(req, res) {}
+function updateTeacherById(req, res) {}
+function deleteTeacherById(req, res) {}
+function createTeacher(req, res) {}
+
+module.exports = {
+  getAllTeachers, 
+  getTeacherById, 
+  updateTeacherById, 
+  deleteTeacherById, 
+  createTeacher
+}
